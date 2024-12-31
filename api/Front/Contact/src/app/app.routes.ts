76f1fd379aa3f1b8ecceaeca62f4e300cca5f14c
mmdrezakarimi <mohammadrezakarimisavadjani@gmail.com>
2024-12-31 10:20:50 -0800

    rout

--- api/Front/Contact/src/app/app.routes.ts
+++ api/Front/Contact/src/app/app.routes.ts
@@ -1,10 +1,10 @@
+import { Routes } from '@angular/router';
 import { ProfilePageComponent } from './Pages/Profile-page/Profile-page.component';
 import { signInPageComponent } from './Pages/SignIn-Page/SignIn-page.component';
-import { Routes } from '@angular/router';
 import { SignUpPageComponent } from './Pages/SignUp-Page/SignUp-page.component';
 
 export const routes: Routes = [
-    {path:'signup', component:SignUpPageComponent},
+    {path:'signUp', component:SignUpPageComponent},
     {path:'signIn', component:signInPageComponent},
     {path:'profile', component:ProfilePageComponent},
 ];
